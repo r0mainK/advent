7ca987cbc07461fc83090cec5cc8dab49abec1fb
Romain Keramitas <r.keramitas@gmail.com>
2019-12-15 11:03:29 +0100

    Update Day 13
    Signed-off-by: Romain Keramitas <r.keramitas@gmail.com>

--- 2019/13/script_part_1.py
+++ 2019/13/script_part_1.py
@@ -52,11 +52,11 @@ def update_base():
     pointers["base"] += program[pointers["parameter_1"]]
 
 
-# if len(sys.argv) != 2:
-#     print("please pass the path to your input file (and nothing else)")
-#     sys.exit(1)
+if len(sys.argv) != 2:
+    print("please pass the path to your input file (and nothing else)")
+    sys.exit(1)
 
-p = Path(__file__).parent / "data.txt"  # Path(sys.argv[1])
+p = Path(__file__).parent / Path(sys.argv[1])
 
 if not p.exists():
     print("%s does not point to a valid input file" % p)
--- 2019/13/script_part_2.py
+++ 2019/13/script_part_2.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import curses
 import operator
 from pathlib import Path
 import sys
@@ -39,7 +40,7 @@ def apply_op(op: Callable[[int, int], int]):
 @update_pointers(pointer_delta=2)
 def acquire_joystick_input():
     print_screen()
-    program[pointers["parameter_1"]] = int(input("enter joystick position:"))
+    program[pointers["parameter_1"]] = int(paddle < ball) - int(paddle > ball)
 
 
 @update_pointers(pointer_delta=2)
@@ -58,27 +59,45 @@ def update_base():
     pointers["base"] += program[pointers["parameter_1"]]
 
 
-def print_screen():
-    sys.stdout.flush()
-    min_x = min(pos[0] for pos in screen)
-    min_y = min(pos[1] for pos in screen)
-    max_x = max(pos[0] for pos in screen)
-    max_y = max(pos[1] for pos in screen)
-    pixel_map = {0: " ", 1: "X", 2: "#", 3: "-", 4: "o"}
-    lines = ""
+def init_screen(printer):
+    def wrapper(**kwargs):
+        if not wrapper.constants:
+            wrapper.constants["min_x"] = min(pos[0] for pos in screen)
+            wrapper.constants["max_x"] = max(pos[0] for pos in screen)
+            wrapper.constants["delta_x"] = (
+                curses.COLS - (wrapper.constants["max_x"] - wrapper.constants["min_x"])
+            ) // 2
+            wrapper.constants["min_y"] = min(pos[1] for pos in screen)
+            wrapper.constants["max_y"] = max(pos[1] for pos in screen)
+            wrapper.constants["delta_y"] = (
+                curses.LINES - (wrapper.constants["max_y"] - wrapper.constants["min_y"])
+            ) // 2
+        return printer(**wrapper.constants, **kwargs)
+
+    wrapper.constants = {}
+    return wrapper
+
+
+@init_screen
+def print_screen(
+    min_x=None, max_x=None, delta_x=None, min_y=None, max_y=None, delta_y=None, wait=False
+):
     for y in range(min_y, max_y + 1):
-        line = ""
         for x in range(min_x, max_x + 1):
-            line += pixel_map[screen[(x, y)]]
-        lines += line + "\n"
-    sys.stdout.write(lines)
+            pixel = pixel_map[screen[(x, y)]]
+            stdscr.addstr(y + delta_y, delta_x + x, pixel, curses.color_pair(screen[(x, y)] + 1))
+    score_string = "SCORE: %d" % score
+    stdscr.addstr(max_y + 1 + delta_y, (curses.COLS - len(score_string)) // 2, score_string, 4)
+    stdscr.refresh()
+    if wait:
+        stdscr.getkey()
 
 
-# if len(sys.argv) != 2:
-#     print("please pass the path to your input file (and nothing else)")
-#     sys.exit(1)
+if len(sys.argv) != 2:
+    print("please pass the path to your input file (and nothing else)")
+    sys.exit(1)
 
-p = Path(__file__).parent / "data.txt"  # Path(sys.argv[1])
+p = Path(__file__).parent / Path(sys.argv[1])
 
 if not p.exists():
     print("%s does not point to a valid input file" % p)
@@ -108,6 +127,16 @@ pointers = {
     "parameter_2": 0,
     "parameter_3": 0,
 }
+stdscr = curses.initscr()
+curses.noecho()
+curses.start_color()
+stdscr.clear()
+curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLACK)
+curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_GREEN)
+curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLACK)
+curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
+pixel_map = {0: " ", 1: " ", 2: " ", 3: "-", 4: "o"}
 screen = defaultdict(int)
 position = []
 score = None
@@ -123,8 +152,13 @@ while program[pointers["instruction"]] != 99:
             score = instruction_output
         else:
             screen[tuple(position)] = instruction_output
+            if instruction_output == 3:
+                paddle = position[0]
+            elif instruction_output == 4:
+                ball = position[0]
         position = []
     else:
         position.append(instruction_output)
-
-print("last score: %d" % score)
+print_screen(wait=True)
+curses.echo()
+curses.endwin()
